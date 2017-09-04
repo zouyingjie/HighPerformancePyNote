b39b27932f34f2c8a3c2a3976816b31b973fca94
zouyingjie <yingjiezou1@gmail.com>
2017-09-04 14:49:08 +0800

    第二章笔记

--- cp02/demo01.py
+++ cp02/demo01.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import time
+import line_profiler
 
 x1, x2, y1, y2 = -1.8, 1.8, -1.8, 1.8
 
@@ -34,8 +35,9 @@ def calc_pure_python(desired_width, max_iterations):
     end_time = time.time()
     secs = end_time - start_time
     print(calculate_z_serial_purepython.__name__ + 'took', secs, "seconds")
-    assert sum(out_put) == 33219980
+    # assert sum(out_put) == 33219980
 
+@profile
 def calculate_z_serial_purepython(maxiter, zs, cs):
     output = [0] * len(zs)
     for i in range(len(zs)):
@@ -59,7 +61,7 @@ def timefn(fn):
         return result
     return measure_time
 if __name__ == "__main__":
-    calc_pure_python(desired_width=1000, max_iterations=300)
+    calc_pure_python(desired_width=100, max_iterations=300)
 
 
 # 使用 timeit 模块
@@ -67,3 +69,36 @@ if __name__ == "__main__":
 
 # 使用 Unix 的系统 time 命令
 # /usr/bin/time -p python demo01.py
+
+
+# 使用 cProfiler 分析函数的执行
+
+# 直接使用 cProfile 进行分析
+#  python -m cProfile -s cumulative cp02/demo01.py
+
+# 将分析结果存储到统计文件中
+# python -m cProfile -o profile.stats cp02/demo01.py
+
+# 操作统计文件
+import pstats
+
+p = pstats.Stats('profile.stats')
+p.sort_stats("cumulative")
+# 输出累计时间报告
+p.print_stats()
+# 输出调用者的信息
+p.print_callers()
+# 输出哪个函数调用了哪个函数
+p.print_callees()
+
+
+# line_profiler 使用
+# 在要测试的函数上添加 @profile 装饰器
+# 使用 kernprof 执行对应的 py 文件或者函数， 会生成 lprof 文件
+# kernprof -l cp02/demo01.py
+#  查看 lprof 文件查看函数每行代码的运行时间
+# python -m line_profiler demo01.py.lprof
+
+# mem_profiler 使用
+# 在要监控的函数上添加 @profile
+# python -m memory_profiler cp02/demo01.py
